5a1b4aad18dadb32e843a664749a10a3429fcd1e
Sebastian Sdorra <s.sdorra@gmail.com>
2016-02-26 21:44:28 +0100

    use autoprefixer to keep sass files small

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,8 +2,14 @@ module.exports = function(gulp, $, info, paths) {
   'use strict';
 
   gulp.task('styles', function(done){
+    var prefixerCfg = {
+      browsers: ["> 0%"],
+      cascade: false
+    };
+
     return gulp.src(paths.styles + '/**/*.scss')
       .pipe($.sass())
+      .pipe($.autoprefixer(prefixerCfg))
       .pipe(gulp.dest(paths.tmp))
   });
 
